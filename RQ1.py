a0a4f8e09880f8b1923e55aac23e5e853f8db467
Mageec3 <2003maciek@gmail.com>
2025-01-07 20:53:17 +0100

    changed RQ1

--- RQ1.py
+++ RQ1.py
@@ -1,10 +1,17 @@
 import json
 import matplotlib.pyplot as plt
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
+# keywords = [" star tracking ", " ephmeris generation ", " telescope control ", " image stacking ", " photometry tools ",
+#             " planetarium mode ", " star charts ", " 3d ", " augmented reality ", " spectroscopy analysis ",
+#             " astrometry ", " light curve ", " radio astronomy ", " satellite tracking ", " meteor shower prediction ",
+#             " augmented reality ", " ar ", " vr ", " virtual reality "]
 
-keywords = ["star tracking", "ephmeris generation", "telescope control", "image stacking", "photometry tools",
-            "planetarium mode", "star charts", "3D models", "augmented reality", "spectroscopy analysis",
-            "astrometry", "light curve", "radio astronomy", "satellite tracking", "meteor shower prediction"]
+keywords = [" sky mapping ", " star identification ", " augmented reality ", " ar ", " vr ", " 3d view ",
+             " time travel ", " night mode ", " satellite tracking ", " meteor shower prediction ", " light pollution map ",
+             " telescope control ",  " assistance ", " observation logs ", " augmented space probes ",
+             " custom overlays ", " skysafari ", " star walk ", " skyview ", " skyguide ", " stellarium ", " night sky "]
+# " zoom ", " sharing "
 
 def check_features(comments, keywords):
     feature_mentions = {keyword: 0 for keyword in keywords}
@@ -20,13 +27,26 @@ def check_features(comments, keywords):
 with open('processed_astronomy_comments.ndjson', 'r', encoding='utf-8') as file:
     processed_comments = json.load(file)
 
+not_processed_comments = []
+with open('astronomy_comments.ndjson', 'r', encoding='utf-8') as file:
+    for line in file:
+        entry = json.loads(line)
+        if entry['author'] != '[deleted]':
+            not_processed_comments.append(entry['body'])
+
 comments = [entry['body'] for entry in processed_comments]
 
 feature_mentions = check_features(comments, keywords)
 
-for feature, count in feature_mentions.items():
-    print(f"{feature}: {count} mentions")
+# for feature, count in feature_mentions.items():
+#     print(f"{feature}: {count} mentions")
 
+# analyzer = SentimentIntensityAnalyzer()
+# for sentence in not_processed_comments[:10]:
+#     vs = analyzer.polarity_scores(sentence)
+#     print("{:-<65} {}".format(sentence, str(vs)))
+
+# exit()
 features = list(feature_mentions.keys())
 counts = list(feature_mentions.values())
 

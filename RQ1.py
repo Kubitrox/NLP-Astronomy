7e30f46ca0c76d7d09c8469a79438d1c064d5192
tomavuc <toma.vucinic@gmail.com>
2025-01-09 20:56:32 +0100

    Update RQ1.py

--- RQ1.py
+++ RQ1.py
@@ -2,15 +2,13 @@ import json
 import matplotlib.pyplot as plt
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-
-
 keywords = [" sky mapping ", " star identification ", " augmented reality ", " ar ", " vr ", " virtual reality ", " 3d view ",
              " time travel ", " night mode ", " satellite tracking ", " meteor shower prediction ", " light pollution map ",
              " telescope control ",  " assistance ", " observation logs ", " augmented space probes ",
              " custom overlays ", " skysafari ", " star walk ", " skyview ", " skyguide ", " stellarium ", " night sky "]
 
 def check_features(processed_comments, keywords):
-    comments = [entry['body'] for entry in processed_comments]
+    comments = [entry['words'] for entry in processed_comments]
 
     feature_mentions = {keyword: 0 for keyword in keywords if keyword not in [" ar ", " augmented reality ", " vr ", " virtual reality "]}
     feature_mentions[" ar/augmented reality "] = 0
@@ -30,7 +28,7 @@ def check_features(processed_comments, keywords):
     return feature_mentions
 
 def calculate_average_compound_scores(processed_comments, keywords, sentiment_scores, feature_mentions):
-    comments = [(entry['body'], entry['id']) for entry in processed_comments]
+    comments = [(entry['words'], entry['id']) for entry in processed_comments]
 
     keyword_compound_scores = {keyword: 0 for keyword in keywords if keyword not in [" ar ", " augmented reality ", " vr ", " virtual reality "]}
     keyword_compound_scores[" ar/augmented reality "] = 0
@@ -56,15 +54,14 @@ def calculate_average_compound_scores(processed_comments, keywords, sentiment_sc
     return keyword_avg_compound_scores
 
 
-with open('processed_astronomy_comments.ndjson', 'r', encoding='utf-8') as file:
+with open('preprocessed_comments_json_format.ndjson', 'r', encoding='utf-8') as file:
     processed_comments = json.load(file)
 
 not_processed_comments = []
 with open('astronomy_comments.ndjson', 'r', encoding='utf-8') as file:
     for line in file:
         entry = json.loads(line)
-        if entry['author'] != '[deleted]':
-            not_processed_comments.append((entry['body'], entry['id']))
+        not_processed_comments.append((entry['body'], entry['id']))
 
 
 vs_id_dict = {}
@@ -80,7 +77,6 @@ sorted_avg_scores = sorted(keyword_avg_compound_scores.items(), key=lambda item:
 for feature, avg_score in sorted_avg_scores:
     print(f"{feature}: Average Compound Score = {avg_score}")
 
-# Plot the sorted average compound scores
 features = [item[0] for item in sorted_avg_scores]
 avg_scores = [item[1] for item in sorted_avg_scores]
 
